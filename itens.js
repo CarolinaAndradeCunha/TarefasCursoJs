e143a480d1db5025068c4f6f2403224fc84d5cc9
CarolinaAndradeCunha <andradecunhacarolina@gmail.com>
2026-01-25 19:51:46 -0300

    Create itens.js

---
+++ itens.js
@@ -0,0 +1,33 @@
+/* if/else */
+let a = 10
+let b = 20
+
+if (a = b) {
+console.log("a é igual a b")
+} else {
+  console.log("a não é igual a b")
+
+/* array */
+
+
+/* function */
+  
+/*objeto*/
+const pessoa1 = {
+  nome: 'Carol',
+  idade: 20,
+}
+
+/*objeto com função*/
+function criarPessoa(nome, idade) {
+return {
+  nome: nome,
+  idade: idade,
+};
+}
+
+const pessoa1 = criarPessoa('Carol', 20);
+console.log(pessoa1.idade);
+
+
+
